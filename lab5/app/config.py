e7cdf6205fed64c1ae1bb7068c1158a331df64ff
Юлія Сенишин <yuliasenyshyn@MacBook-Air-Ulia.local>
2025-09-29 22:24:01 +0300

    Fix type hints for Python 3.9

--- lab5/app/config.py
+++ lab5/app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Yulia2006.@127.0.0.1:3306/gym_2'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://gym_user:Yulia2006.@gym-database.caxii84umkba.us-east-1.rds.amazonaws.com:3306/gym_2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- lab5/app/dao/trainers_has_users_dao.py
+++ lab5/app/dao/trainers_has_users_dao.py
@@ -1,8 +1,9 @@
 from app.domain import TrainersHasUsers
 from app.dao.general_dao import GeneralDAO
+from typing import Optional
 
 class TrainerHasUserDAO(GeneralDAO):
     _domain_type = TrainersHasUsers
 
-    def add_trainer_user(trainer_name: str, user_email: str, start_date: str, end_date: str | None = None):
+    def add_trainer_user(trainer_name: str, user_email: str, start_date: str, end_date: Optional[str] = None):
         return TrainersHasUsers.insert_record(trainer_name, user_email, start_date, end_date)
