a769d4c76b8ab1d64c234a2f003b74f2718ad6bd
Юлія Сенишин <yuliasenyshyn@MacBook-Air-Ulia.local>
2025-10-06 10:34:02 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from app import create_app
 
 app = create_app()
 
+print("Flask app reloaded successfully!")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
